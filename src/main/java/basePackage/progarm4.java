415d6def230456bccaf5d75acdbd9fc6d1fd91ed
harshal <aherharshal91@gmail.com>
2026-04-16 19:09:01 +0530

    new chnages added in program

---
+++ src/main/java/basePackage/progarm4.java
@@ -0,0 +1,80 @@
+package basePackage;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
+import java.util.List;
+
+public class progarm4 {
+    public static  void main (String[] args)
+    {
+        WebDriverManager.chromiumdriver().setup();
+        WebDriver driver =new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.get("https://testautomationpractice.blogspot.com/");
+        driver.findElement(By.xpath("//input[@placeholder='Enter Name']")).sendKeys("Aher");
+        driver.findElement(By.xpath("//input[@placeholder='Enter EMail']")).sendKeys("abc@gamil.com");
+        driver.findElement((By.xpath("//input[@placeholder='Enter Phone']"))).sendKeys("134252626");
+        driver.findElement(By.id("textarea")).sendKeys("Pune");
+        driver.findElement(By.id("male")).click();
+        //count all checkbox
+        List<WebElement> checkboxes=driver.findElements(By.xpath("//div[@class='form-group']//input[@type='checkbox']"));
+        System.out.println(checkboxes.size());
+        for(WebElement check:checkboxes)
+        {
+            String value = check.getAttribute("value");
+            if(value.equalsIgnoreCase("Monday")) {
+                check.click();
+                break;
+            }
+        }
+        // drop down list
+        WebElement Cddl=driver.findElement(By.id("country"));
+        Select ddl= new Select(Cddl);
+        ddl.selectByIndex(2);
+
+        WebElement clo=driver.findElement(By.id("colors"));
+        Select c =new Select(clo);
+        c.selectByValue("yellow");
+
+        //calendar
+
+        driver.findElement(By.id("datepicker")).click();
+        driver.findElement(By.xpath("//span[@class='ui-icon ui-icon-circle-triangle-e']")).click();
+        driver.findElement(By.xpath("//a[text()='16']")).click();
+
+        //manually enter value in calendar
+        WebElement date=driver.findElement(By.id("txtDate"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].removeAttribute('readonly')", date);
+        // Set the date value
+        date.sendKeys("21/09/2026");
+        //calendar with drop down
+
+        WebElement date1=driver.findElement(By.id("start-date"));
+        date1.sendKeys("16-4-2026");
+        driver.findElement(By.id("end-date")).sendKeys("20-4-2026");
+        driver.findElement(By.className("submit-btn")).click();
+
+        //upload single file
+        driver.findElement(By.id("singleFileInput")).sendKeys("C://Users//Gayatri//Downloads//Harshal_SDET.docx");
+        WebElement file= driver.findElement(By.id("multipleFilesForm"));
+        String filePath1 = "C:\\Users\\Gayatri\\Downloads\\Harshal_SDET.docx";
+        String filePath2 = "C:\\Users\\Gayatri\\Downloads\\Harshal_Ramkrushna_Aher.docx";
+
+        fileInput.sendKeys(filePath1 + "\n" + filePath2 + "\n");
+        driver.findElement(By.xpath("//button[text()='Upload Multiple Files']")).click();
+    
+
+
+
+    }
+}
+
